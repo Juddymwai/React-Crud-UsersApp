0e8e85b366cb32cd4efb6e8e5667a006a2e2ae21
Judy Mwai <judymukami1996@gmail.com>
2022-11-15 09:28:53 +0300

    adds user add form

--- src/AddUser.js
+++ src/AddUser.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
+
+import {FormGroup, Form, Label, Input, Button} from "reactstrap"
 
 export const AddUser = () => {
 
   return (
     <div>
-        <h1>Add user</h1>
+        <Form>
+            <FormGroup>
+                <Label>Name</Label>
+                <Input type="text" placeholder='Enter Name'></Input>
+            </FormGroup>
+            <Button type="submit">Submit</Button>
+            <Link to="/" className='btn btn-danger ml-2'>Cancel</Link>
+        </Form>
     </div>
   )
 }
